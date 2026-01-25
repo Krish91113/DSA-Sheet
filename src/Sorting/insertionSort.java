5b98636790012e422bbf278248810d58d293bf0f
Krish Baranwal <menikabaranwal121@gmail.com>
2026-01-25 09:26:08 +0530

    sorting done

--- src/Sorting/insertionSort.java
+++ src/Sorting/insertionSort.java
@@ -1,5 +1,20 @@
 package Sorting;
 
 public class insertionSort {
+    public static void main(String[] args) {
+        int arr[] = {13,46,24,52,20,9};
+        for(int i=0;i<arr.length;i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
 
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
 }
