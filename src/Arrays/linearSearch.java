51fe4cb146d1097bea2284a8cd30630647b032d1
Krish Baranwal <menikabaranwal121@gmail.com>
2026-02-04 09:10:53 +0530

    missing number done

---
+++ src/Arrays/linearSearch.java
@@ -0,0 +1,24 @@
+package Arrays;
+import java.util.*;
+public class linearSearch {
+    private static int linearSearches(int nums[], int target){
+        int ans =0;
+        for(int i=0;i< nums.length;i++){
+            if(nums[i] == target){
+                ans =i;
+                return ans;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i< nums.length;i++){
+            nums[i]=sc.nextInt();
+        }
+        int target =sc.nextInt();
+        System.out.println(linearSearches(nums, target));
+    }
+}
---
+++ src/Arrays/missingNumber.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class missingNumber {
+    private static int missingNo(int nums[]){
+        int tSum=0;
+        int n =nums.length;
+        int sum=(n*(n+1)/2);
+        for(int i=0;i<nums.length;i++){
+             tSum +=nums[i];
+        }
+        return sum-tSum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i< nums.length;i++){
+            nums[i]=sc.nextInt();
+        }
+        System.out.println(missingNo(nums));
+    }
+}
