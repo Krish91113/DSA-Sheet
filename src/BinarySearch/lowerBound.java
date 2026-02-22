c21a6786a55a5d11bf0ab51122ce03b094feb4cd
Krish Baranwal <menikabaranwal121@gmail.com>
2026-02-22 21:54:59 +0530

    lower bound done

--- src/BinarySearch/lowerBound.java
+++ src/BinarySearch/lowerBound.java
@@ -1,4 +1,29 @@
 package BinarySearch;
 
+import java.util.Scanner;
+
 public class lowerBound {
+    public static int lowBound(int nums[],int x){
+        int lb=0,i=0,j=nums.length-1;
+        while(i<=j){
+            int mid = i +(j - i)/2;
+            if(nums[mid]>=x){
+                lb =mid;
+                j=mid-1;
+            }else {
+                i=mid+1;
+            }
+        }
+        return lb;
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i< nums.length;i++){
+            nums[i]=sc.nextInt();
+        }
+        int x =sc.nextInt();
+        System.out.println(lowBound(nums,x));
+    }
 }
