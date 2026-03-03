a6ea9c355ccaf1aaa00383b8b5391413305a1820
Krish Baranwal <menikabaranwal121@gmail.com>
2026-03-03 13:56:04 +0530

    search IN rotated arrays 2 done

--- src/BinarySearch/searchInRotatedSortedArray2.java
+++ src/BinarySearch/searchInRotatedSortedArray2.java
@@ -2,32 +2,34 @@ package BinarySearch;
 
 import java.util.Scanner;
 
-public class searchInRotatedSortedArray {
-    private static int searchInArray(int nums[], int x){
-        int i=0,j=nums.length-1,ans= 0;
+public class searchInRotatedSortedArray2 {
+    private static boolean searchInArray(int nums[], int x){
+        int i=0,j=nums.length-1;
         while(i<=j){
-            int mid=i+(j-i)/2;
-            if(nums[mid]== x){
-                ans =mid;
-                return ans;
+            int mid =i+(j-i)/2;
+            if(nums[mid] == x){
+                return true;
             }
-            if(nums[mid]>=nums[i]){
-                if(nums[mid]<= x && x < nums[i]){
+            if(nums[i]==nums[mid] && nums[mid] == nums[j]){
+                i++;
+                j--;
+                continue;
+            }
+            if(nums[i]<= nums[mid]){
+                if(nums[i]<= x && x <= nums[mid]){
                     j=mid-1;
                 }else{
                     i=mid+1;
                 }
             }else{
-                if(nums[mid]<= x && x < nums[i]){
-                    j=mid-1;
-                }else{
+                if(nums[mid]<= x && x<= nums[j]){
                     i=mid+1;
+                }else{
+                    j=mid-1;
                 }
             }
-
         }
-        return ans;
-
+        return false;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
