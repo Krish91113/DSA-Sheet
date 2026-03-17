68811b7f9082b41cd4060acf5227fa0c5f3200bb
Krish Baranwal <menikabaranwal121@gmail.com>
2026-03-17 21:33:32 +0530

    done with one q

---
+++ src/BinarySearch/OnAnswer/findSqrtOfNumber.java
@@ -0,0 +1,24 @@
+package BinarySearch.OnAnswer;
+
+import java.util.Scanner;
+
+public class findSqrtOfNumber {
+    private static int findsqrt(int n){
+        int low =0,high=n,ans= 0;
+        while(low<= high){
+            int mid = low + high /2 ;
+            if(mid * mid <= n){
+                ans =mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(findsqrt(n));
+    }
+}
