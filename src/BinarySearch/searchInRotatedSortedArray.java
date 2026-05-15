a16994f6cb15a5da0e2b325c77c52c04c12c46b8
Krish Baranwal <menikabaranwal121@gmail.com>
2026-05-15 08:54:02 +0530

    solve question

--- src/BinarySearch/searchInRotatedSortedArray.java
+++ src/BinarySearch/searchInRotatedSortedArray.java
@@ -4,30 +4,34 @@ import java.util.Scanner;
 
 public class searchInRotatedSortedArray {
     private static int searchInArray(int nums[], int x){
-        int i=0,j=nums.length-1,ans= 0;
-        while(i<=j){
-            int mid=i+(j-i)/2;
-            if(nums[mid]== x){
-                ans =mid;
-                return ans;
-            }
-            if(nums[mid]>=nums[i]){
-                if(nums[mid]<= x && x < nums[i]){
-                    j=mid-1;
-                }else{
-                    i=mid+1;
-                }
-            }else{
-                if(nums[mid]<= x && x < nums[i]){
-                    j=mid-1;
-                }else{
-                    i=mid+1;
-                }
+        int i = 0, j = nums.length - 1;
+
+        while(i <= j){
+            int mid = i + (j - i) / 2;
+
+            if(nums[mid] == x){
+                return mid;
             }
 
+            // Left half is sorted
+            if(nums[i] <= nums[mid]){
+                if(nums[i] <= x && x < nums[mid]){
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if(nums[mid] < x && x <= nums[j]){
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            }
         }
-        return ans;
 
+        return -1;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
