dc44c2395e8312cc8ecc7dc7f8a636a8c052dfab
Krish Baranwal <menikabaranwal121@gmail.com>
2026-06-03 07:21:01 +0530

    segregate odd and even done

--- src/LinkedList/SingleLinkedList/checkifPalindrome.java
+++ src/LinkedList/SingleLinkedList/checkifPalindrome.java
@@ -1,61 +1,61 @@
-package LinkedList.SingleLinkedList;
-import java.util.*;
-class Node {
-    int data;
-    Node next;
-
-    Node(int val) {
-        data = val;
-        next = null;
-    }
-}
-public class checkifPalindrome {
-    public static Node findMid(Node head){
-        Node slow =head;
-        Node fast=head;
-        while(fast!=null && fast.next!= null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-    public static boolean checkPalindrome(Node head){
-        if(head == null || head.next == null) return true;
-        Node mid =findMid(head);
-        Node prev =null;
-        Node curr=mid;
-        while(curr!= null){
-            Node next =curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-        curr=head;
-        while(prev!= null){
-            if(prev.data != curr.data) return false;
-            prev=prev.next;
-            curr=curr.next;
-        }
-        return true;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Node head = null;
-        Node tail = null;
-        System.out.println("Enter Number of Elements");
-        int n = sc.nextInt();
-        System.out.println("Enter elements");
-        for (int i = 0; i < n; i++) {
-            int val = sc.nextInt();
-            Node newNode = new Node(val);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        System.out.println(checkPalindrome(head));
-    }
-}
+//package LinkedList.SingleLinkedList;
+////import java.util.*;
+////class Node {
+////    int data;
+////    Node next;
+////
+////    Node(int val) {
+////        data = val;
+////        next = null;
+////    }
+////}
+//public class checkifPalindrome {
+//    public static Node findMid(Node head){
+//        Node slow =head;
+//        Node fast=head;
+//        while(fast!=null && fast.next!= null){
+//            slow=slow.next;
+//            fast=fast.next.next;
+//        }
+//        return slow;
+//    }
+//    public static boolean checkPalindrome(Node head){
+//        if(head == null || head.next == null) return true;
+//        Node mid =findMid(head);
+//        Node prev =null;
+//        Node curr=mid;
+//        while(curr!= null){
+//            Node next =curr.next;
+//            curr.next=prev;
+//            prev=curr;
+//            curr=next;
+//        }
+//        curr=head;
+//        while(prev!= null){
+//            if(prev.data != curr.data) return false;
+//            prev=prev.next;
+//            curr=curr.next;
+//        }
+//        return true;
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        Node head = null;
+//        Node tail = null;
+//        System.out.println("Enter Number of Elements");
+//        int n = sc.nextInt();
+//        System.out.println("Enter elements");
+//        for (int i = 0; i < n; i++) {
+//            int val = sc.nextInt();
+//            Node newNode = new Node(val);
+//            if (head == null) {
+//                head = newNode;
+//                tail = newNode;
+//            } else {
+//                tail.next = newNode;
+//                tail = newNode;
+//            }
+//        }
+//        System.out.println(checkPalindrome(head));
+//    }
+//}
---
+++ src/LinkedList/SingleLinkedList/segregateOddAndEvenNodes.java
@@ -0,0 +1,59 @@
+package LinkedList.SingleLinkedList;
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+}
+public class segregateOddAndEvenNodes {
+    public static void evenAndOdd(Node head){
+        Node odd =new Node(-1);
+        Node even=new Node(-1);
+        Node temp=head;
+        Node t1=odd;
+        Node t2=even;
+        while(temp!=null){
+            int c =temp.data;
+            if(c % 2 != 0){
+                t1.next=temp;
+                t1=t1.next;
+            }else{
+                t2.next=temp;
+                t2=t2.next;
+            }
+            temp=temp.next;
+        }
+        t2.next=null;
+        odd=odd.next;
+        t1.next=even.next;
+        Node z=odd;
+        while(z!=null){
+            System.out.print(z.data + "->");
+            z=z.next;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        Node tail = null;
+        System.out.println("Enter Number of Elements");
+        int n = sc.nextInt();
+        System.out.println("Enter elements");
+        for (int i = 0; i < n; i++) {
+            int val = sc.nextInt();
+            Node newNode = new Node(val);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        evenAndOdd(head);
+    }
+}
