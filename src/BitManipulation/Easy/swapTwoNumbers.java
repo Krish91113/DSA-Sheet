be1986b0a0e2c0ec9446eb4e903830833b69bc63
Krish Baranwal <menikabaranwal121@gmail.com>
2026-06-08 08:18:55 +0530

    swap of tow no done

---
+++ src/BitManipulation/Easy/swapTwoNumbers.java
@@ -0,0 +1,17 @@
+package BitManipulation.Easy;
+import java.util.*;
+public class swapTwoNumbers {
+    public static void swap(int a, int b){
+        a = a ^ b;
+        b= a ^ b;
+        a=a ^ b;
+        System.out.println("a" + "=" + a);
+        System.out.println("b" + "=" + b);
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        swap(a,b);
+    }
+}
