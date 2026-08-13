c22bcdda7caf5f1c6dc24918d5af85be69f9b585
Krish Baranwal <menikabaranwal121@gmail.com>
2026-08-13 19:49:33 +0530

    graph started

--- src/Graph/dfsTraversal.java
+++ src/Graph/dfsTraversal.java
@@ -1,4 +1,64 @@
 package Graph;
 
+import java.util.*;
+
 public class dfsTraversal {
+
+    public static void dfs(
+            int node,
+            boolean[] vis,
+            ArrayList<ArrayList<Integer>> adj,
+            ArrayList<Integer> ls) {
+
+        // Mark current node visited
+        vis[node] = true;
+
+        // Add current node to answer
+        ls.add(node);
+
+        // Visit all neighbours
+        for(int n : adj.get(node)) {
+
+            if(!vis[n]) {
+                dfs(n, vis, adj, ls);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int V = sc.nextInt();
+        int E = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        // Since nodes are 1 to V
+        for(int i = 0; i <= V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Build undirected graph
+        for(int i = 0; i < E; i++) {
+
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        int node = 1;
+
+        boolean[] vis = new boolean[V + 1];
+
+        ArrayList<Integer> ls = new ArrayList<>();
+
+        dfs(node, vis, adj, ls);
+
+        for(int i = 0; i < ls.size(); i++) {
+            System.out.print(ls.get(i) + " ");
+        }
+    }
 }
--- src/Graph/numberofProvinces.java
+++ src/Graph/numberofProvinces.java
@@ -1,4 +1,36 @@
 package Graph;
-
+import java.util.*;
 public class numberofProvinces {
+    class Solution {
+        public static void bfs(int i, int adj[][], boolean vis[]){
+            vis[i]=true;
+            int n=adj.length;
+            Queue<Integer> q=new LinkedList<>();
+            q.add(i);
+            while(q.size()>0){
+                int front=q.remove();
+                for(int j=0;j<n;j++){
+                    if(adj[front][j]==1 && vis[j]==false){
+                        q.add(j);
+                        vis[j]=true;
+                    }
+                }
+            }
+        }
+        public int findCircleNum(int[][] adj) {
+            int n=adj.length;
+            int c=0;
+            boolean visited[] = new boolean[n];
+            for(int i=0;i<n;i++){
+                if(!visited[i]){
+                    bfs(i,adj,visited);
+                    c++;
+                }
+            }
+            return c;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
 }
